2ff097072ddd490a0dddc3fbb52dda7f6d2b5068
zhaokeke <313976385@qq.com>
2014-01-19 15:47:26 +0800

    update test.java

--- src/com/git/Test.java
+++ src/com/git/Test.java
@@ -2,6 +2,7 @@ package com.git;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello git");
+		System.out.println("hello git9");
+		System.out.println("test git");
 	}
 }
